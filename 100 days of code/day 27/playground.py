0fa70463410ba62a14492bf81fb80d48098e2d8e
Mayank Kumar <52621155+innovativecreations@users.noreply.github.com>
2022-12-09 22:41:52 +0530

    Delete 100 days of code directory

--- 100 days of code/day 27/playground.py
+++
@@ -1,6 +0,0 @@
-def nacho(*arg):
-    a = 0
-    for i in arg:
-        a += i
-    print(a)
-nacho(2,3,6)
--- 100 days of code/quiz-game-start/question_model.py
+++
@@ -1,4 +0,0 @@
-class Question:
-    def __init__(self, questions, answers):
-        self.text = questions
-        self.ans = answers
